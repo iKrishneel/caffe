f772fe69bdf1522d71e70450fd3b918cb145ceb5
Krishneel <krishneel.chand.chaudhary@gmail.com>
2016-12-04 21:49:18 +0900

    another disect

--- jobs/mbzirc_truck_detector/train_truck_detector.sh
+++ jobs/mbzirc_truck_detector/train_truck_detector.sh
@@ -3,4 +3,4 @@
 cd $CAFFE_ROOT
 build/tools/caffe train --solver=jobs/mbzirc_truck_detector/solver.prototxt \
     --gpu=0 \
-    --weights=/home/krishneel/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel \
+    --weights=/home/krishneel/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel
