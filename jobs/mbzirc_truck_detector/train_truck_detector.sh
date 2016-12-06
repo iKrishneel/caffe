775df21184ad4bf67c35230d731e6b1255694056
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2016-12-06 20:29:25 +0900

    finetune

--- jobs/mbzirc_truck_detector/train_truck_detector.sh
+++ jobs/mbzirc_truck_detector/train_truck_detector.sh
@@ -3,5 +3,5 @@
 cd $CAFFE_ROOT
 build/tools/caffe train --solver=jobs/mbzirc_truck_detector/solver.prototxt \
     --gpu=0 \
-    --weights=jobs/mbzirc_truck_detector/weights/snapshot_iter_14200.caffemodel
+    --weights=jobs/mbzirc_truck_detector/weights/snapshot_iter_22801.caffemodel
 ## --weights=/home/krishneel/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel
