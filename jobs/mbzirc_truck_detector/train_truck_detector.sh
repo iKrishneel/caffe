d2b6644bb02fb8f360fbd86d7b4fc444e3f9ea50
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2016-12-07 20:23:50 +0900

    trained fisheye images

--- jobs/mbzirc_truck_detector/train_truck_detector.sh
+++ jobs/mbzirc_truck_detector/train_truck_detector.sh
@@ -3,5 +3,6 @@
 cd $CAFFE_ROOT
 build/tools/caffe train --solver=jobs/mbzirc_truck_detector/solver.prototxt \
     --gpu=0 \
-    --weights=jobs/mbzirc_truck_detector/weights/snapshot_iter_22801.caffemodel
-## --weights=/home/krishneel/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel
+    --weights=/home/krishneel/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel
+    ##--weights=jobs/mbzirc_truck_detector/train3_weights/snapshot_iter_7800.caffemodel
+
